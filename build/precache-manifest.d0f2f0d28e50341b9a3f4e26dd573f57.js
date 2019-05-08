79d607f14d79adcdfa8682d5984cbfc6eff05d1c
robert-barcelona <robertanthony.bcn@gmail.com>
2019-05-08 19:26:55 +0200

    push to github

---
+++ build/precache-manifest.d0f2f0d28e50341b9a3f4e26dd573f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf78372c792b0ef2d713b04389cce27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "206e445146e4b7de7578",
+    "url": "/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "839af48dd1bd71fdca34",
+    "url": "/static/css/main.d4c7a9eb.chunk.css"
+  },
+  {
+    "revision": "206e445146e4b7de7578",
+    "url": "/static/js/2.d58c47ac.chunk.js"
+  },
+  {
+    "revision": "839af48dd1bd71fdca34",
+    "url": "/static/js/main.cf7ff3e2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
